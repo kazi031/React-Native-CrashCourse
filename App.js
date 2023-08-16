3a7f168d6dfa1a5c6dbe46330c4170201c720e03
kazi031 <kazirafidraiyan031@gmail.com>
2023-08-16 16:05:42 +0600

    Lesson 6 FlatList3

---
+++ App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
+
+  return (
+    <View style={styles.container}>
+
+    <FlatList 
+      key={'&'}
+      keyExtractor={(item) => '&' + item.id}
+      data={people}
+      renderItem={({ item }) => (
+        <Text style={styles.item}>{item.name}</Text>
+      )}
+      numColumns={2}
+    />
+
+    {/* FlatList is better in performance. It loads as it scrolls down but 
+    scrollView Loads at a time. */}
+
+    {/* <ScrollView>
+      { people.map(item => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+    </ScrollView> */}
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
+  },
+});
