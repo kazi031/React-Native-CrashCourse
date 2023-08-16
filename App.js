8055ed3a1463fa2c1381c2c7a0557338782f94a9
kazi031 <kazirafidraiyan031@gmail.com>
2023-08-16 15:01:16 +0600

    Lesson 3 Text Input

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  const [name, setName] = useState('Shaun');
+  const [age, setAge] = useState('30');
+
+  return (
+    <View style={styles.container}>
+      <Text>Enter name:</Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val) => setName(val)}
+        />
+
+      <Text>Enter age:</Text>
+      <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 25'
+        onChangeText={(val) => setAge(val)}
+        />
+      <Text>name: {name}, age: {age}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  }
+});
